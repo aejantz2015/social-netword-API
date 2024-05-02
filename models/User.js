569df9c44a7da7499f82eb2872697c5f2ebb95fc
annajantzen <aejantz2015@gmail.com>
2024-05-02 18:23:19 -0500

    added user model

---
+++ models/User.js
@@ -0,0 +1,46 @@
+const { Schema, model, Types } = require('mongoose')
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: [
+                /^[\w-]+(\.[\w-]+)*@[\w-]+(\.[\w-]+)+$/,
+                "Please fill in a valid email address",
+            ],
+        },
+        thougths: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought',
+            },
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User',
+            },
+        ],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+    }
+)
+
+userSchema.virtual('freindCount').get(function(){
+    return this.friends.length
+})
+
+const User = model('User', userSchema)
+
+module.exports = User
